2c1d47a2841472c108a5c96c198dde9238a4317b
czy0538 <czy0538@gmail.com>
2019-02-26 16:51:59 +0800

    1007

--- PTA-cpp/Source.cpp
+++ PTA-cpp/Source.cpp
@@ -40,3 +40,62 @@ int main()
     return 0;
 }
 */
+
+//1007 素数对猜想 （20 分）
+int main()
+{
+    int num[1229];
+    int n;
+    cin >> n;
+    int j;
+    int flag = -1;
+    for (int i = 2; i !=n; ++i)
+    {
+        for (j = 2; j != i; j++)
+        {
+            if (i % j == 0)
+                break;
+        }
+        if (j == i)
+            num[++flag] = i;
+    }
+    int count = 0;
+    for (int i=0; i != flag; ++i)
+    {
+        if ((num[i+1] - num[i]) == 2)
+            ++count;
+    }
+    cout << count << endl;
+    return 0;
+}
+
+//1009 说反话
+/*
+int main()
+{
+    vector<char> in;
+    string temp;
+    getline(cin,temp);
+    auto size = temp.length;
+    for (int i = size, j = 0; i != -1; --i)
+    {
+        while (temp[i] != ' ')
+            j++;
+        if (temp[i] = ' ')
+        {
+            for (int k = 0; k != j; ++k)
+            {
+                in.push_back(temp[i + k]);
+                if (k == j - 1)
+                {
+                    in.push_back(' ');
+                }
+            }
+
+        }
+    }
+    cout << temp;
+    return 0;
+
+}
+*/
